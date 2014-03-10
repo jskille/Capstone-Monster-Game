778b8b6fef81aaa0bf0f28bc823904afca090d3f
ASanti <Darkstars31@gmail.com>
2014-03-10 01:22:23 -0500

    AIController Script Added Basic AI Movement implemented. To disable uncheck AIController script connected with the cube entity.

---
+++ Assets/AIController.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var creaturePos = this.transform.position;
+var moveFlag = 0;
+var loiter = 1;
+
+function Start () {
+
+		
+}
+
+function Update () {
+	
+	// Moves the player to the right
+	if (this.transform.position.x < 30 ) {moveFlag = 1; }
+		else if (this.transform.position.x > 55 )
+		 				{ moveFlag = 0; }
+		 		
+				if (moveFlag == 1) moveRight();
+				else moveLeft();
+}
+
+function moveRight()
+{
+	this.transform.position.x = this.transform.position.x + .05;
+}
+
+function moveLeft()
+{
+	this.transform.position.x = this.transform.position.x - .05;
+}
