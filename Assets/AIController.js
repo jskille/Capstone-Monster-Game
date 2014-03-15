ee9857b63ffff853c9f383e6e2e68d6eabd57127
ASanti <Darkstars31@gmail.com>
2014-03-15 03:23:31 -0500

    Adjusted the AIController to better handle the future implementation of AIStates. Small bug fixes. Added comments to code. Further reduced the amount of spaghetti style coding into a more refined method based solution. Fixed small bug where AI would take over if the user was holding down a key for a certain length of time, AI now waits the proper amount of time to resume.

--- Assets/AIController.js
+++ Assets/AIController.js
@@ -1,16 +1,20 @@
 ﻿#pragma strict
 
-//var creaturePos = this.transform.position;
+
 var moveFlag : int = 0;
 var loiter : int = 1;
 var cMoveSpeed : float = 0.5;
 var DecisionTimer = 700;
-var Chance = 0;
+var Chance : float = 0;
 var disableAItime = 0; // Disables AI during input
 private var motor : CharacterMotor;
 private var cameraMotor : CharacterMotor;
 var autoRotate : boolean = true;
 var maxRotationSpeed : float = 360;
+
+// AI State Logic
+enum AIState { Asleep, Idling, Loiting, Walking, Running, Sitting, Chasing, Fleeing, HavingLunch };
+var CurrentState = null;
 
 function Start () {
 		motor = GetComponent(CharacterMotor);
@@ -20,61 +24,62 @@ function Start () {
 function Update () {
 		 				
 	// Puts AI on pause			
-	if (Input.anyKeyDown)
+	if (Input.anyKey)
 		{	disableAItime = 500; }
-		
-		
-				// Moves the player to the right
+				
+				// AI Movement Bounderies
 				if (this.transform.position.x < 25 ) {moveFlag = 1; }
 								else if (this.transform.position.x > 200 ){ moveFlag = 0; }
-		
-		
+			
 	// Puts AI on pause during input
 	if (disableAItime == 0){
 	
+							AIControlCenter();
+														
+	// Allows AI to take over again
+	}else {disableAItime--;}
+		
+}
+//////////////////////////////////////////////////////////////////
+function AIControlCenter (){
+
 							// AI Logic Must GO HERE		
 							if ( DecisionTimer == 0) {										
 													// AI Makes a Choice//
 													//////////////////////
-													Chance = Random.Range(1,100);
+													Chance = AIChoice(1,100);
 													
 													if (Chance > 85) changeDirection(moveFlag);
 													
 													DecisionTimer = 500; //Resets the timer for next decision		
-													}
+							}
 													
 							/*** AI Logic State Chance ***/
-							if (Chance < 24 ) { loiter = 1; 
-												} else {loiter = 0; }
-
-							
-							
+							if (Chance < 24 ) { loiter = 1; CurrentState = AIState.Loiting;
+												} else { loiter = 0; CurrentState = AIState.Walking;
+							}
+				
 							/*** AI Logic Action ***/
 							if (loiter == 0){ // Stops AI, Forces Cube to loiter in one area
 							
 									if (moveFlag == 1) { moveRight();} //Moves the Cube Right
 										else if (moveFlag == 0 ){ moveLeft();} //Movies the Cube Left
-								}
-											 
-							
-								
-								
-								
+							}
+		
 							DecisionTimer--; // Forces creature to make a decision
-							
-							
-	// Allows AI to take over again
-	}else {disableAItime--;}
+
+} // End AIControlCenter
+function AIChoice (x : float,y : float) {
+		
+		// Current minor implementation, will expand later
+		return Random.Range(x,y);
 	
-	
-	
-	
-}
+} // End AIChoice
 
 function changeDirection(x){
 				if ( x == 1) moveFlag = 0;
 				if ( x == 0) moveFlag = 1;
-}
+} // End changeDirection
 
 function moveRight()
 {
@@ -102,6 +107,7 @@ function moveLeft()
 	cameraMotor.inputMoveDirection = new Vector3(-cMoveSpeed, 0, 0);
 }
 
+// Functions NOT in Use at the moment
 function moveAway()
 {
 	this.transform.position.z = this.transform.position.z + cMoveSpeed;
