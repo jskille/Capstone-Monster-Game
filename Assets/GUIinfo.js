bcd01b594ca1297d770424b33e4caee70588ef93
Anthony M Santi <Darkstars31@gmail.com>
2014-03-19 16:44:15 -0500

    Slight Changes to GUI
    Created variable for GUI height so it can be adjusted easily.
    GUI buttons now expand into the extended menus.

--- Assets/GUIinfo.js
+++ Assets/GUIinfo.js
@@ -8,6 +8,8 @@ private var Ball : createBall;
 var hungryOffset : float;
 var thirstyOffset : float;
 
+var ButtonHeight : int = 30;
+
 function Start () {
 	showPlaces = false;
 	creatureActions = false;
@@ -19,67 +21,68 @@ function Start () {
 function OnGUI() {
 	if (!showPlaces)
 	{
-		if (GUI.Button(Rect(0,0,100,50), "Show Places")){
+		if (GUI.Button(Rect(0,0,100,ButtonHeight - 10), "Show Places")){
 			showPlaces = true;
 		}
 	}
 	else
 	{
-		if(GUI.Button(Rect(0, 0, 100, 50), "Hide Places")){
+		if(GUI.Button(Rect(0, 0, 100, ButtonHeight), "Hide Places")){
 			showPlaces = false;
 		}
-		if (GUI.Button(Rect(100,0,100,50), "Mini-game 1")){
+		if (GUI.Button(Rect(100,0,100,ButtonHeight), "Mini-game 1")){
 			print("Go to Minigame 1");
 		}
-		if (GUI.Button(Rect(200, 0, 100, 50), "Mini-game 2")){
+		if (GUI.Button(Rect(200, 0, 100, ButtonHeight), "Mini-game 2")){
 			print("Go to Minigame 2");
 		}
-		if (GUI.Button(Rect(300, 0, 100, 50), "Mini-game 3")){
+		if (GUI.Button(Rect(300, 0, 100, ButtonHeight), "Mini-game 3")){
 			print("Go to Minigame 3");
 		}
-		if (GUI.Button(Rect(400, 0, 100, 50), "Mini-game 4")){
+		if (GUI.Button(Rect(400, 0, 100, ButtonHeight), "Mini-game 4")){
 			print("Go to Minigame 4");
 		}
-		if (GUI.Button(Rect(500, 0, 100, 50), "Creature Shop")){
+		if (GUI.Button(Rect(500, 0, 100, ButtonHeight), "Creature Shop")){
 			print("Creature Shop");
 		}
-		if (GUI.Button(Rect(600, 0, 100, 50), "Multiplayer")){
+		if (GUI.Button(Rect(600, 0, 100, ButtonHeight), "Multiplayer")){
 			print("Go to Multiplayer");
 		}
 	}
 	
 	if((!creatureActions) && (!playMenu))
 	{
-		if(GUI.Button(Rect(0, Screen.height-50, 100, 50), "Show Actions")){
+		if(GUI.Button(Rect(0, Screen.height-30, 100, ButtonHeight - 10), "Show Actions")){
 			creatureActions = true;
 		}
 	}
 	else if (!playMenu)
 	{
-		if(GUI.Button(Rect(0, Screen.height-50, 100, 50), "Hide Actions")){
+		if(GUI.Button(Rect(0, Screen.height-30, 100, ButtonHeight), "Hide Actions")){
 			creatureActions = false;
 		}
-		if(GUI.Button(Rect(100, Screen.height-50, 100, 50), "Feed Creature")){
+		if(GUI.Button(Rect(100, Screen.height-30, 100, ButtonHeight), "Feed Creature")){
 			print("Fed Creature");
 			AIControl.hungry = false;
 		}
-		if(GUI.Button(Rect(200, Screen.height-50, 100, 50), "Get Water")){
+		if(GUI.Button(Rect(200, Screen.height-30, 100, ButtonHeight), "Get Water")){
 			print("Creature got drink");
 			AIControl.thirsty = false;
 		}
-		if(GUI.Button(Rect(300, Screen.height-50, 150,50), "Play with Creature")){
+		if(GUI.Button(Rect(300, Screen.height-30, 150,ButtonHeight), "Play with Creature")){
 			playMenu = true;
 			//AIControl.bored = false;
 		}
 	}
 	else
 	{
-		if (GUI.Button(Rect(0, Screen.height-50, 100, 50), "Back to Actions")){
+		if (GUI.Button(Rect(0, Screen.height-30, 100, ButtonHeight), "Back to Actions")){
 			playMenu = false;
 		}
-		if (GUI.Button(Rect(100, Screen.height-50, 100, 50), "Ball")){
+		if (GUI.Button(Rect(100, Screen.height-30, 100, ButtonHeight), "Ball")){
 			playMenu = false;
-			Ball.makeBall(this.transform.position.x+10, this.transform.position.y, this.transform.position.z);
+			Ball.makeBall(this.transform.position.x, this.transform.position.y+25, this.transform.position.z);
+			
 		}
 	}
 	
