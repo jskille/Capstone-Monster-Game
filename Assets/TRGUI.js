065a20efd7ffcbaa3e4cb8341dbfd26f4e7ecf43
Alex DeBeer <addbq2@mail.missouri.edu>
2014-03-27 15:55:33 -0500

    templerun gui + other shit

---
+++ Assets/TRGUI.js
@@ -0,0 +1,54 @@
+﻿#pragma strict
+
+var ButtonHeight : int = 30;
+var showQuitMenu : boolean = false;
+var objectiveCube : int = 0;
+var objectiveCubeName : String = "";
+
+function Start () {
+
+}
+
+function OnGUI() {
+
+//if user is x level: set objcube to 0
+	if(objectiveCube == 0)
+	{
+		objectiveCubeName = "Green";
+		GUI.Button(Rect(500,0,300,ButtonHeight - 10), "Current Objective " + objectiveCubeName);
+	}
+	else if(objectiveCube == 1)
+	{
+		objectiveCubeName = "Blue";
+		GUI.Button(Rect(500,0,300,ButtonHeight - 10), "Current Objective " + objectiveCubeName);
+	}
+	else if(objectiveCube == 2)
+	{
+		objectiveCubeName = "White";
+		GUI.Button(Rect(500,0,300,ButtonHeight - 10), "Current Objective " + objectiveCubeName);
+	}
+	
+	if(!showQuitMenu)
+	{
+		if(GUI.Button(Rect(0,0,100,ButtonHeight - 10), "Quit Game"))
+		{
+			showQuitMenu = true;
+		}
+
+	}
+	else
+	{
+		if(GUI.Button(Rect(0,0,100,ButtonHeight - 10), "Cancel"))
+		{
+			showQuitMenu = false;
+		}
+		if(GUI.Button(Rect(100,0,100,ButtonHeight - 10), "Quit"))
+		{
+			Application.LoadLevel(0);
+		}
+	}
+}
+
+function Update () {
+
+}
---
+++ Assets/doIAppear.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var cubeNumber : int = 0;
+
+function Start () {
+
+	//get user level.
+	var userLevel : int = 0;
+	
+	if(userLevel <= 5)
+	{//1-5
+		if((cubeNumber == 1) || (cubeNumber == 2))
+		{
+			renderer.enabled = false;
+			collider.enabled = false;
+		}
+	}
+	else if(userLevel <= 10 && userLevel > 5)
+	{//6-10
+		if(cubeNumber == 0 || cubeNumber == 2)
+		{
+			renderer.enabled = false;
+			collider.enabled = false;
+		}
+	}
+	else if(userLevel > 10)//11+
+	{
+		if(cubeNumber == 0 || cubeNumber == 1)
+		{
+			renderer.enabled = false;
+			collider.enabled = false;
+		}
+	}
+}
+
+function Update () {
+
+}
